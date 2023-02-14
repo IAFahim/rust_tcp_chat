dc3c6af4957ceeedf2697f84855c311428f130ef
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-02-14 13:19:42 +0600

    Works

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,31 @@ use tokio::sync::broadcast;
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:9111").await.unwrap();
-    let ( sender, _) = broadcast::channel::<String>(10);
+    let (sender, _) = broadcast::channel::<String>(10);
     loop {
         let (mut tcp_steam, addr) = listener.accept().await.unwrap();
-        let sender=sender.clone();
-        let mut receiver=sender.subscribe();
+        let sender = sender.clone();
+        let mut receiver = sender.subscribe();
         tokio::spawn(async move {
-            let (read, mut write) = tcp_steam.split();
-            let mut buffer_reader = BufReader::new(read);
+            let (reader_Half, mut write_half) = tcp_steam.split();
+            let mut buffer_reader = BufReader::new(reader_Half);
             let mut line = String::new();
             loop {
-                let size = buffer_reader.read_line(&mut line).await.unwrap();
-                if size==0{
-                   break;
+                tokio::select! {
+                    n = buffer_reader.read_line(&mut line) => {
+                        if n.unwrap() == 0 {
+                            break;
+                        }
+                        let _ = sender.send(line.clone());
+                        line.clear();
+                    }
+
+                    msg=receiver.recv()=>{
+                        let msg: String=msg.unwrap();
+                        let _ = write_half.write_all(msg.as_bytes()).await.unwrap();
+                    }
+
                 }
-
-                sender.send(line.clone()).unwrap();
-                let msg= receiver.recv().await.unwrap();
-                write.write_all(msg.as_bytes()).await.unwrap();
-
-                line.clear();
             }
         });
     }
