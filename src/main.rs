91ffc834b7b0810023396cad376a18860bc79a37
Md. Ishtiaq Ahamed Fahim <ishtiaq.fahim@northsouth.edu>
2023-02-14 14:55:46 +0600

    Can send message between peoples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::net::SocketAddr;
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
 use tokio::sync::broadcast;
@@ -5,14 +6,14 @@ use tokio::sync::broadcast;
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:9111").await.unwrap();
-    let (sender, _) = broadcast::channel::<String>(10);
+    let (sender, _) = broadcast::channel::<(String,SocketAddr)>(10);
     loop {
         let (mut tcp_steam, addr) = listener.accept().await.unwrap();
         let sender = sender.clone();
         let mut receiver = sender.subscribe();
         tokio::spawn(async move {
-            let (reader_Half, mut write_half) = tcp_steam.split();
-            let mut buffer_reader = BufReader::new(reader_Half);
+            let (reader_half, mut write_half) = tcp_steam.split();
+            let mut buffer_reader = BufReader::new(reader_half);
             let mut line = String::new();
             loop {
                 tokio::select! {
@@ -20,16 +21,18 @@ async fn main() {
                         if n.unwrap() == 0 {
                             break;
                         }
-                        let _ = sender.send(line.clone());
+                        let _ = sender.send((line.clone(), addr));
                         line.clear();
                     }
 
                     msg=receiver.recv()=>{
-                        let msg: String=msg.unwrap();
-                        let _ = write_half.write_all(msg.as_bytes()).await.unwrap();
+                        let (msg, other_addr)=msg.unwrap();
+                        if other_addr!=addr {
+                            let _ = write_half.write_all(msg.as_bytes()).await.unwrap();
+                        }
                     }
-
                 }
+
             }
         });
     }
